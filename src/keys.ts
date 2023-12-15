54898ba62ef1eb892e6a6bc768d2d16c86711874
Roberto Difilippo <robertoruben.difilippo.contractor@gdevtools.com>
2023-12-15 09:37:26 -0300

    Cambio en credenciales bbdd

--- src/keys.ts
+++ src/keys.ts
@@ -1,3 +1,21 @@
+// export default {
+//     database: {
+//         host: 'db4free.net',
+//         user: 'appequipos',
+//         password: '928z2k3jt6vm3lkkgyo9',
+//         database: 'app_equipos'
+//     }
+// }
+
+// export default {
+//     database: {
+//         host: 'localhost',
+//         user: 'root',
+//         password: 'root',
+//         database: 'app_equipos'
+//     }
+// }
+
 export default {
     database: {
         host: 'db4free.net',
